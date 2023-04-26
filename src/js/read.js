eb4d4648a45aa11446662491df861a73d24157a5
BiezdarJS <piotrdeja.pl@gmail.com>
2023-04-26 21:03:25 +0200

    changes to read.js file

--- src/js/read.js
+++ src/js/read.js
@@ -2,5 +2,20 @@ import { users$ } from './collection.js';
 
 
 users$.then(users => {
-  console.log(users);
+  printUsers(users);
 });
+
+
+function printUsers(users) {
+
+ users.forEach(user => {
+
+    const usersList = document.querySelector('.users-list');
+    const newListItem = document.createElement('li');
+    newListItem.classList.add('users-item');
+    newListItem.innerHTML = user.name;
+    usersList.appendChild(newListItem);
+
+ });
+ 
+} 
